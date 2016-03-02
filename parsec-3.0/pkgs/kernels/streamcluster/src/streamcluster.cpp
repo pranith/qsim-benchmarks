1cbeae439ed71029832e5d07488e27f66f6cde43
Pranith Kumar <bobby.prani@gmail.com>
2016-03-02 16:43:52 -0500

    Convert streamcluster to new qsim API

--- parsec-3.0/pkgs/kernels/streamcluster/src/streamcluster.cpp
+++ parsec-3.0/pkgs/kernels/streamcluster/src/streamcluster.cpp
@@ -36,35 +36,6 @@ using namespace tbb;
 #include <hooks.h>
 #endif
 
-/***** QSIM CHECKPOINT *****/
-#ifdef QSIM
-#define APP_START() do { \
-  __asm__ __volatile__("cpuid;"::"a"(0xaaaaaaaa));\
-    } while(0)
-
-#define APP_END() do { \
-  __asm__ __volatile__("cpuid;"::"a"(0xfa11dead));\
-    } while(0)
-#else
-    unsigned long long start_time;
-    unsigned long long end_time;
-
-#include <sys/time.h>
-
-static inline unsigned long long usec_time(void) {
-      unsigned long long usec;
-        struct timeval tv;
-          gettimeofday(&tv, NULL);
-            usec = 1000000 * tv.tv_sec + tv.tv_usec;
-}
-
-#define APP_START() do { start_time = usec_time(); } while(0)
-#define APP_END() do { \
-  end_time = usec_time(); \
-    printf("%lluus\n", end_time - start_time); \
-      } while(0)
-#endif
-
 using namespace std;
 
 #define MAXNAMESIZE 1024 // max filename length
@@ -2051,10 +2022,6 @@ int main(int argc, char **argv)
     stream = new FileStream(infilename);
   }
 
-#ifdef QSIM
-    APP_START();
-#endif
-
 #ifdef ENABLE_PARSEC_HOOKS
   __parsec_roi_begin();
 #endif
@@ -2065,10 +2032,6 @@ int main(int argc, char **argv)
   __parsec_roi_end();
 #endif
   
-#ifdef QSIM
-    APP_END();
-#endif
-
   delete stream;
 
 #ifdef ENABLE_PARSEC_HOOKS
